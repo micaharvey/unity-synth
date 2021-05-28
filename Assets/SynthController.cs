05f65a087a7539a7147bfc8792a7e83979365702
Micah Arvey <micah@recroom.com>
2021-05-28 12:10:40 -0700

    black keys

--- Assets/SynthController.cs
+++ Assets/SynthController.cs
@@ -113,8 +113,6 @@ public class SynthController : MonoBehaviour
         }
 
         // check for keyboard events
-        if (Input.GetKeyDown(KeyCode.Space)) Play();
-        if (Input.GetKeyUp(KeyCode.Space)) Off();
         if (Input.GetKeyDown(KeyCode.Z)) Play(48);
         if (Input.GetKeyUp(KeyCode.Z)) Off(48);
         if (Input.GetKeyDown(KeyCode.X)) Play(50);
@@ -135,6 +133,21 @@ public class SynthController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Period)) Off(62);
         if (Input.GetKeyDown(KeyCode.Slash)) Play(64);
         if (Input.GetKeyUp(KeyCode.Slash)) Off(64);
+
+        if (Input.GetKeyDown(KeyCode.S)) Play(49);
+        if (Input.GetKeyUp(KeyCode.S)) Off(49);
+        if (Input.GetKeyDown(KeyCode.D)) Play(51);
+        if (Input.GetKeyUp(KeyCode.D)) Off(51);
+        if (Input.GetKeyDown(KeyCode.G)) Play(54);
+        if (Input.GetKeyUp(KeyCode.G)) Off(54);
+        if (Input.GetKeyDown(KeyCode.H)) Play(56);
+        if (Input.GetKeyUp(KeyCode.H)) Off(56);
+        if (Input.GetKeyDown(KeyCode.J)) Play(58);
+        if (Input.GetKeyUp(KeyCode.J)) Off(58);
+        if (Input.GetKeyDown(KeyCode.L)) Play(61);
+        if (Input.GetKeyUp(KeyCode.L)) Off(61);
+        if (Input.GetKeyDown(KeyCode.Semicolon)) Play(63);
+        if (Input.GetKeyUp(KeyCode.Semicolon)) Off(63);
 
         if (Input.GetKeyDown(KeyCode.Q)) Play(48 + 12);
         if (Input.GetKeyUp(KeyCode.Q)) Off(48 + 12);
@@ -164,5 +177,22 @@ public class SynthController : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.Backslash)) Off(69 + 12);
         if (Input.GetKeyDown(KeyCode.Pipe)) Play(69 + 12);
         if (Input.GetKeyUp(KeyCode.Pipe)) Off(69 + 12);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2)) Play(49 + 12);
+        if (Input.GetKeyUp(KeyCode.Alpha2)) Off(49 + 12);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) Play(51 + 12);
+        if (Input.GetKeyUp(KeyCode.Alpha3)) Off(51 + 12);
+        if (Input.GetKeyDown(KeyCode.Alpha5)) Play(54 + 12);
+        if (Input.GetKeyUp(KeyCode.Alpha5)) Off(54 + 12);
+        if (Input.GetKeyDown(KeyCode.Alpha6)) Play(56 + 12);
+        if (Input.GetKeyUp(KeyCode.Alpha6)) Off(56 + 12);
+        if (Input.GetKeyDown(KeyCode.Alpha7)) Play(58 + 12);
+        if (Input.GetKeyUp(KeyCode.Alpha7)) Off(58 + 12);
+        if (Input.GetKeyDown(KeyCode.Alpha9)) Play(61 + 12);
+        if (Input.GetKeyUp(KeyCode.Alpha9)) Off(61 + 12);
+        if (Input.GetKeyDown(KeyCode.Alpha0)) Play(63 + 12);
+        if (Input.GetKeyUp(KeyCode.Alpha0)) Off(63 + 12);
+        if (Input.GetKeyDown(KeyCode.Equals)) Play(65 + 12);
+        if (Input.GetKeyUp(KeyCode.Equals)) Off(65 + 12);
     }
 }
